9e432c34b1ffa0efab991940e5e0c855f294bca6
Harisankar P S <mailme@hsps.in>
2014-08-15 14:22:40 +0530

    temp fix

--- app/uploaders/bootsy/image_uploader.rb
+++ app/uploaders/bootsy/image_uploader.rb
@@ -2,34 +2,24 @@
 module Bootsy
   class ImageUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
-
-    storage Bootsy.storage
-
-    def store_dir
-      "#{Bootsy.store_dir}/#{model.class.to_s.underscore}/#{model.id}"
+  
+    process :tags => ["bootsy_image", "#{Apartment::Tenant.current}"]
+    process :convert => "jpg"
+  
+    version :thumbnail do
+      eager
+      resize_to_fit(300, 300)
+      cloudinary_transformation :quality => 90
     end
-
-    # Process files as they are uploaded:
-    process resize_to_limit: [1160, 2000]
-
-    version :large do
-      process resize_to_fit: [Bootsy.large_image[:width], Bootsy.large_image[:height]]
-    end
-
-    version :medium do
-      process resize_to_fit: [Bootsy.medium_image[:width], Bootsy.medium_image[:height]]
-    end
-
-    version :small do
-      process resize_to_fit: [Bootsy.small_image[:width], Bootsy.small_image[:height]]
-    end
-
-    version :thumb do
-      process resize_to_fill: [60, 60]
-    end
-
+  
     def extension_white_list
       %w(jpg jpeg gif png)
     end
+  
+  
+    def public_id
+      return "#{Apartment::Tenant.current}/" + model.name
+    end
+
   end
 end
